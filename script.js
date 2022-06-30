4dd158c263f710aa06b37af70259e950d6f26582
ThanhMai <mai.ltt.62cntt@ntu.edu.vn>
2022-06-30 13:51:42 +0700

    306

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.slide1').slick({
+        slidesToShow: 3,
+        infinite: true,
+        dots: true,
+        arrows:false,
+    });
+
+    $('.slide0').slick({
+        slidesToShow: 6,
+        infinite: true,
+        dots: true,
+        arrows:false,
+     });
+});
