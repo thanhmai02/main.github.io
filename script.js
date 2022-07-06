6771d199bdd8a299f97bf0e4aa802df8de97419c
ThanhMai <mai.ltt.62cntt@ntu.edu.vn>
2022-07-06 17:28:52 +0700

    607

--- script.js
+++ script.js
@@ -1,28 +1,117 @@
-$(document).ready(function(){
-    $('.slide1').slick({
-        slidesToShow: 3,
-        infinite: true,
-        dots: true,
-        arrows:false,
-    });
+$(document).ready(function () {
+   $('.slide1').slick({
+      slidesToShow: 3,
+      infinite: true,
+      dots: true,
+      arrows: false,
+      responsive: [
+         {
+            breakpoint: 991,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 767,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 575,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            }
+         },
+      ]
+   });
 
-    $('.slide0').slick({
-        slidesToShow: 6,
-        infinite: true,
-        dots: false,
-        arrows:false,
-     });
+   $('.slide0').slick({
+      slidesToShow: 6,
+      infinite: true,
+      dots: false,
+      arrows: false,
+      responsive: [
+         {
+            breakpoint: 991,
+            settings: {
+               slidesToShow: 5,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 767,
+            settings: {
+               slidesToShow: 4,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 575,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+            }
+         },
+      ]
 
-     $('.main-infor').slick({
-        slidesToShow: 3,
-        infinite: true,
-        dots: true,
-        arrows:false,
-     });
-     $('.natural').slick({
-        slidesToShow: 3,
-        infinite: true,
-        dots: true,
-        arrows:false,
-     });
+   });
+
+
+
+   $('.main-infor').slick({
+      slidesToShow: 3,
+      infinite: true,
+      dots: true,
+      arrows: false,
+      responsive: [
+         {
+            breakpoint: 991,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 767,
+            settings: {
+               infinite: false,
+               slidesToShow: 1.3,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 575,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            }
+         },
+      ]
+   });
+   $('.natural').slick({
+      slidesToShow: 3,
+      infinite: true,
+      dots: true,
+      arrows: false,
+      responsive: [
+         {
+            breakpoint: 991,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 767,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            }
+         },
+      ]
+   });
 });
